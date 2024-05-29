62407bdbf29f1823d23deaf037e96c1971695bb0
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-05-29 17:34:47 +0500

    feat: offchain tasks execution

--- src/commands/execute/index.ts
+++ src/commands/execute/index.ts
@@ -4,6 +4,7 @@ import { fetcher } from "../../fetcher";
 import { CmdInfoSupplier } from "../types";
 import { CombinedConfigResolver, DEFAULT_CONFIG_PATH } from "../../config";
 import { TaskExecutor } from "../../executor/tasks";
+import { OffchainExecutor } from "../../executor/offchain";
 
 export const ExecuteCommandInfo: CmdInfoSupplier = (cli) =>
   cli.addCmd({
@@ -19,6 +20,9 @@ export const ExecuteCommandInfo: CmdInfoSupplier = (cli) =>
       }),
       configFile: namedParam(types.string.withDefaultValue(""), {
         description: "Optional different execution params file",
+      }),
+      dryRun: namedParam(types.booleanFlag, {
+        description: "Just show execution plan",
       }),
     },
     getData: (args) => ({
@@ -39,13 +43,21 @@ export const ExecuteCommandInfo: CmdInfoSupplier = (cli) =>
         const instructions = await parse(steps, configResolver);
 
         const tasks = new TaskExecutor();
+        const offchain = new OffchainExecutor();
 
-        console.log("Instructions for execution");
-        console.log(JSON.stringify(instructions, null, 2));
-
-        for (let index = 0; index < instructions.length; index++) {
-          const instruction = instructions[index];
-          await tasks.runStage(args.formula, instruction);
+        if (args.dryRun) {
+          console.log("Instructions for execution");
+          console.log(JSON.stringify(instructions, null, 2));
+        } else {
+          for (let index = 0; index < instructions.length; index++) {
+            const instruction = instructions[index];
+            if (instruction.type === "task") {
+              await tasks.runStage(args.formula, instruction);
+            }
+            if (instruction.type === "offchain") {
+              await offchain.runStage(args.formula, instruction);
+            }
+          }
         }
       },
     }),
--- src/executor/offchain.ts
+++ src/executor/offchain.ts
@@ -3,7 +3,7 @@ import { StageInstruction } from "engine";
 import { getFormulaPath } from "../fetcher";
 import { normalize, join } from "path";
 
-export class TaskExecutor {
+export class OffchainExecutor {
   private readonly docker: Dockerode;
 
   constructor(dockerOpts?: Dockerode.DockerOptions) {
@@ -12,13 +12,17 @@ export class TaskExecutor {
 
   async runStage(formulaPath: string, stage: StageInstruction) {
     const workdir = normalize(join(getFormulaPath(formulaPath)));
+    const Binds = [`${workdir}:/project:ro`];
+    if ("dind" in stage && stage.dind) {
+      Binds.push("/var/run/docker.sock:/var/run/docker.sock");
+    }
     await this.docker.run(stage.image, stage.cmd, process.stdout, {
       Env: Object.entries(stage.envs)
         .map(([name, val]) => `${name}=${val}`)
         .concat(`DREW_WORKDIR=${stage.workdir}`),
       AttachStdout: true,
       AttachStderr: true,
-      HostConfig: { AutoRemove: true, Binds: [`${workdir}:/project:ro`] },
+      HostConfig: { AutoRemove: true, Binds },
     });
   }
 }
