47edc24b8d80061bdf65a74374c846537eeb4b3b
Serafim Gerasimov <serafimgerasimov44@gmail.com>
2024-06-05 21:55:27 +0500

    ci: buildscripts

---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+import path from "path";
+import { defineConfig } from "rollup";
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import { builtinModules } from "module";
+import terser from "@rollup/plugin-terser";
+import shebang from "rollup-plugin-add-shebang";
+
+export default defineConfig({
+  input: "src/index.ts",
+  output: {
+    file: path.resolve("bin", "cli.js"),
+    format: "es",
+  },
+  external: [
+    ...builtinModules,
+    "@drewpackages/engine",
+    "commander",
+    "dockerode",
+    "fs-extra",
+    "memory-streams",
+    "simple-git",
+  ],
+  plugins: [
+    nodeResolve(),
+    json(),
+    typescript({
+      exclude: "node_modules",
+    }),
+    terser(),
+    shebang({
+      include: path.resolve("bin", "cli.js"),
+      shebang: "#!/usr/bin/env node",
+    }),
+  ],
+  watch: {
+    clearScreen: true,
+    include: ["src/**/*.ts", "package.json"],
+  },
+});
